64dedde0f95573214b6261758aa3e0b82af1a781
v786 <jahangirali786@gmail.com>
2018-07-10 22:53:36 +0530

    Added urls and created views

--- swigger/diners/urls.py
+++ swigger/diners/urls.py
@@ -3,6 +3,15 @@ from django.urls import path
 
 from . import views
 
+app_name = 'diners'
+
 urlpatterns = [
-  path('', views.index, name = 'index'),
+  # ex: /diners/
+  path('', views.index, name='index'),
+  # ex: /diners/5/
+  path('<int:diner_id>/', views.detail, name='detail'),
+  # ex: /diners/5/results/
+  path('<int:diner_id>/results/', views.results, name='results'),
+  # ex: /diners/5/vote/
+  path('<int:diner_id>/vote/', views.vote, name='vote'),
 ]
--- swigger/diners/views.py
+++ swigger/diners/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+from .models import Diner
 
 # Create your views here.
 
 def index(request):
-  return HttpResponse("Hello world ! You are at Diners.")
-
+  latest_diner_list = Diner.objects.order_by('-published_date')[:5]
+  context = {'latest_diner_list': latest_diner_list}
+  return render(request, 'diners/index.html', context)
 def detail(request, diner_id):
-    return HttpResponse("You're looking at diner %s." % diner_id)
+    try:
+        diner = Diner.objects.get(pk=diner_id)
+    except Diner.DoesNotExist:
+        raise Http404("Diner does not exist")
+    return render(request, 'diners/detail.html', {'diner': diner})
 
 def results(request, diner_id):
     response = "You're looking at the results of diner %s."
